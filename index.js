a5d184b84b18f1ef59e76e78a24420871f7705ca
HiraArain <hiraarain100@gmail.com>
2024-08-11 02:56:03 +0500

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express= require("express");
 const app = express();
 
 app.get("/", (req, res)=>{
-    res.json({message: "hello from docker"});
+    res.json({message: "hello world"});
 });
 
 app.listen(9000,()=>console.log("sever runing at port 9000"));
