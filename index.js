e3b944b6e154b00042de421fdfd8712901845011
HiraArain <hiraarain100@gmail.com>
2024-08-11 22:07:42 +0500

    changed job

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express= require("express");
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.json({message: "hello world"});
+app.get("/", (req, res) => {
+    res.json({ message: "hello world" });
 });
 
-app.listen(9000,()=>console.log("sever runing at port 9000"));
+app.listen(9000, () => console.log("sever runing at port 9000"));
 
